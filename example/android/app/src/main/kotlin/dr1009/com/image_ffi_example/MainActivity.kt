5d53fd90aa7d4e22446faab5e417b85bbdbff618
Koji Wakamiya <koji.wakamiya@gmail.com>
2025-12-07 16:55:27 +0900

    Create project

---
+++ example/android/app/src/main/kotlin/dr1009/com/image_ffi_example/MainActivity.kt
@@ -0,0 +1,5 @@
+package dr1009.com.image_ffi_example
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
---
+++ ios/Classes/image_ffi.c
@@ -0,0 +1,3 @@
+// Relative import to be able to reuse the C sources.
+// See the comment in ../image_ffi.podspec for more information.
+#include "../../src/image_ffi.c"
