f2545558e3dee5499ee74d71c0079f11d7218363
Koji Wakamiya <koji.wakamiya@gmail.com>
2025-12-14 12:40:15 +0900

    refactor: Code cleanup

--- example/integration_test/app_test.dart
+++ example/integration_test/app_test.dart
@@ -1,5 +1,4 @@
-import 'package:flutter/foundation.dart'
-    show kIsWeb, defaultTargetPlatform, TargetPlatform;
+import 'package:flutter/foundation.dart';
 import 'package:flutter/services.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:image/image.dart' as img;
